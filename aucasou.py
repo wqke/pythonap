1a8c60e9e08e8175a77fac01c63e33e5e8bd3867
wqke <47601667+wqke@users.noreply.github.com>
2019-02-25 21:54:33 +0100

    Update aucasou.py

--- aucasou.py
+++ aucasou.py
@@ -287,33 +287,150 @@ Output('frame-graph', 'figure'),
 def drawevent(selection):
         if selection is None:
                 return {}
+        
         else:
                 i=selection['points'][0]['pointNumber']
                 PV_X,PV_Y,PV_Z=(df['B_Ori_z_TRUE'][i],df['B_Ori_x_TRUE'][i],df['B_Ori_y_TRUE'][i])
                 B_X,B_Y,B_Z=(df['B_End_z_TRUE'][i],df['B_End_x_TRUE'][i],df['B_End_y_TRUE'][i])
+                Dst_X,Dst_Y,Dst_Z=(df['Dst_End_z_TRUE'][i],df['Dst_End_x_TRUE'][i],df['Dst_End_y_TRUE'][i])
+                tau_X,tau_Y,tau_Z=[df['Tau_End_z_TRUE'][i],df['Tau_End_x_TRUE'][i],df['Tau_End_y_TRUE'][i]]
+                D0_X,D0_Y,D0_Z=[df['D0_End_z_TRUE'][i],df['D0_End_x_TRUE'][i],df['D0_End_y_TRUE'][i]]
+                nu_X=df['B_nu_PZ_TRUE'][i]*10000/df['B_nu_P_TRUE'][i]+B_X
+                nu_Y=df['B_nu_PX_TRUE'][i]*10000/df['B_nu_P_TRUE'][i]+B_Y
+                nu_Z=df['B_nu_PY_TRUE'][i]*10000/df['B_nu_P_TRUE'][i]+B_Z
+                K_X=df['D0_K_PZ_TRUE'][i]*10000/df['D0_K_P_TRUE'][i]+D0_X
+                K_Y=df['D0_K_PX_TRUE'][i]*10000/df['D0_K_P_TRUE'][i]+D0_Y
+                K_Z=df['D0_K_PY_TRUE'][i]*10000/df['D0_K_P_TRUE'][i]+D0_Z
+                piK_X=df['D0_Pi_PZ_TRUE'][i]*10000/df['D0_Pi_P_TRUE'][i]+D0_X
+                piK_Y=df['D0_Pi_PX_TRUE'][i]*10000/df['D0_Pi_P_TRUE'][i]+D0_Y
+                piK_Z=df['D0_Pi_PY_TRUE'][i]*10000/df['D0_Pi_P_TRUE'][i]+D0_Z
+                piDst_X=df['Dst_Pi_PZ_TRUE'][i]*10000/df['Dst_Pi_P_TRUE'][i]+Dst_X
+                piDst_Y=df['Dst_Pi_PX_TRUE'][i]*10000/df['Dst_Pi_P_TRUE'][i]+Dst_Y
+                piDst_Z=df['Dst_Pi_PY_TRUE'][i]*10000/df['Dst_Pi_P_TRUE'][i]+Dst_Z
+
+                pitau1_X=df['Tau_Pi1_PZ_TRUE'][i]*10000/df['Tau_Pi1_P_TRUE'][i]+tau_X
+                pitau1_Y=df['Tau_Pi1_PX_TRUE'][i]*10000/df['Tau_Pi1_P_TRUE'][i]+tau_Y
+                pitau1_Z=df['Tau_Pi1_PY_TRUE'][i]*10000/df['Tau_Pi1_P_TRUE'][i]+tau_Z
+
+                pitau2_X=df['Tau_Pi2_PZ_TRUE'][i]*10000/df['Tau_Pi2_P_TRUE'][i]+tau_X
+                pitau2_Y=df['Tau_Pi2_PX_TRUE'][i]*10000/df['Tau_Pi2_P_TRUE'][i]+tau_Y
+                pitau2_Z=df['Tau_Pi2_PY_TRUE'][i]*10000/df['Tau_Pi2_P_TRUE'][i]+tau_Z
+                pitau3_X=df['Tau_Pi3_PZ_TRUE'][i]*10000/df['Tau_Pi3_P_TRUE'][i]+tau_X
+                pitau3_Y=df['Tau_Pi3_PX_TRUE'][i]*10000/df['Tau_Pi3_P_TRUE'][i]+tau_Y
+                pitau3_Z=df['Tau_Pi3_PY_TRUE'][i]*10000/df['Tau_Pi3_P_TRUE'][i]+tau_Z
+                nutau_X=df['Tau_nu_PZ_TRUE'][i]*10000/df['Tau_nu_P_TRUE'][i]+tau_X
+                nutau_Y=df['Tau_nu_PX_TRUE'][i]*10000/df['Tau_nu_P_TRUE'][i]+tau_Y
+                nutau_Z=df['Tau_nu_PY_TRUE'][i]*10000/df['Tau_nu_P_TRUE'][i]+tau_Z
+
+                [traceB,traceDst,tracetau,traceD0,tracenuB,traceK]=[go.Scatter3d(x=[PV_X,B_X],y=[PV_Y,B_Y],z=[PV_Z,B_Z],mode='lines+markers+text',marker=dict(size=5,color= "rgb(5,200,5)", opacity=0.8),text=['PV', ''],textposition='top left',line = dict(color = ('rgb(0, 0, 255)'),width = 3)),go.Scatter3d(x=[B_X,Dst_X],y=[B_Y,Dst_Y],z=[B_Z,Dst_Z],mode='lines+markers+text',marker=dict(size=5,color= "rgb(5,200,5)",opacity=0.8),text=['', 'D*'],textposition='top left',line = dict(color = ('rgb(0, 0, 255)'),width = 3)),go.Scatter3d(x=[B_X,tau_X],y=[B_Y,tau_Y],z=[B_Z,tau_Z],mode='lines+markers+text',marker=dict(size=5,color= "rgb(5,200,5)", opacity=0.8),text=['', 'tau'],textposition='top left',line = dict(color = ('rgb(0, 0, 255)'),width = 3)),go.Scatter3d(x=[Dst_X,D0_X],y=[Dst_Y,D0_Y],z=[Dst_Z,D0_Z],mode='lines+markers+text',marker=dict(size=5,color= "rgb(5,200,5)", opacity=0.8),text=['', 'D0'],textposition='top left',line = dict(color = ('rgb(0, 0, 255)'),width = 3)),go.Scatter3d(x=[B_X,nu_X],y=[B_Y,nu_Y],z=[B_Z,nu_Z],mode='lines+markers+text',marker=dict(size=5,color= "rgb(5,200,5)",opacity=0.8),text=['', 'nuB'],textposition='top left',line = dict(color = ('rgb(0, 0, 255)'),width = 3)),go.Scatter3d(x=[D0_X,K_X],y=[D0_Y,K_Y],z=[D0_Z,K_Z],mode='lines+markers+text',marker=dict(size=5,color= "rgb(5,200,5)", opacity=0.8),text=['', 'K'],textposition='top left',line = dict(color = ('rgb(0, 0, 255)'),width = 3))]
+
+        
+                tracepiK=go.Scatter3d(x=[D0_X,piK_X],y=[D0_Y,piK_Y],z=[D0_Z,piK_Z],mode='lines+markers+text',marker=dict(size=5,color= "rgb(5,200,5)", opacity=0.8),text=['', 'pi'],textposition='top left',line = dict(color = ('rgb(0, 0, 255)'),width = 3))
+
+                tracepiDst=go.Scatter3d(x=[Dst_X,piDst_X],y=[Dst_Y,piDst_Y],z=[Dst_Z,piDst_Z],mode='lines+markers+text',marker=dict(size=5,color= "rgb(5,200,5)", opacity=0.8),text=['', 'pi'],textposition='top left',line = dict(color = ('rgb(0, 0, 255)'),width = 3))
+
+                tracepitau1=go.Scatter3d(x=[tau_X,pitau1_X],y=[tau_Y,pitau1_Y],z=[tau_Z,pitau1_Z],mode='lines+markers+text',marker=dict(size=5,color= "rgb(5,200,5)", opacity=0.8),text=['', 'pi'],textposition='top left',line = dict(color = ('rgb(0, 0, 255)'),width = 3))
+
+                tracepitau2=go.Scatter3d(x=[tau_X,pitau2_X],y=[tau_Y,pitau2_Y],z=[tau_Z,pitau2_Z],mode='lines+markers+text',marker=dict(size=5,color= "rgb(5,200,5)", opacity=0.8),text=['', 'pi'],textposition='top left',line = dict(color = ('rgb(0, 0, 255)'),width = 3))
 
 
-                traceB=go.Scatter3d(
-                        x=[PV_X,B_X],
-                        y=[PV_Y,B_Y],
-                        z=[PV_Z,B_Z],
-                        mode='lines+markers+text',
-                        marker=dict(
-                                size=5,
-                                color= "rgb(5,200,5)",
-                                opacity=0.8),
-                        text=['PV', ''],
-                        textposition='top left',
-                        line = dict(
-                                color = ('rgb(0, 0, 255)'),
-                                width = 3)
+                tracepitau3=go.Scatter3d(x=[tau_X,pitau3_X],y=[tau_Y,pitau3_Y],z=[tau_Z,pitau3_Z],mode='lines+markers+text',marker=dict(size=5,color= "rgb(5,200,5)", opacity=0.8),text=['', 'pi'],textposition='top left',line = dict(color = ('rgb(0, 0, 255)'),width = 3))
+
+
+                tracenutau=go.Scatter3d(x=[tau_X,nutau_X],y=[tau_Y,nutau_Y],z=[tau_Z,nutau_Z],mode='lines+markers+text',marker=dict(size=5,color= "rgb(5,200,5)", opacity=0.8),text=['', 'nu'],textposition='top left',line = dict(color = ('rgb(0, 0, 255)'),width = 3))
+
+
+
+                xrange=max(abs([PV_X,B_X,Dst_X,D0_X,tau_X,K_X,nutau_X,nu_X,pitau1_X,
+                                pitau2_X,pitau3_X,piDst_X,piK_X]))
+                yrange=max(abs([PV_Y,B_Y,Dst_Y,D0_Y,tau_Y,K_Y,nutau_Y,nu_Y,pitau1_Y,
+                         pitau2_Y,pitau3_Y,piDst_Y,piK_Y]))
+
+                zrange=max(abs([PV_Z,B_Z,Dst_Z,D0_Z,tau_Z,K_Z,nutau_Z,nu_Z,pitau1_Z,
+                                pitau2_Z,pitau3_Z,piDst_Z,piK_Z]))
+
+
+
+                layout_event = go.Layout(
+                showlegend=False,
+
+                width=400,
+                height=450,
+                autosize=False,
+                margin=dict(t=0, b=0, l=0, r=0),
+                scene=dict(
+                        xaxis=dict(
+                        range=[-abs(xrange), abs(xrange)],
+                        title='Z direction [mm]',
+                        titlefont=dict(
+                        family='Arial, sans-serif',
+                        size=18,
+                        color='black'
+                        ),
+                        gridcolor='#bdbdbd',
+                        gridwidth=2,
+                        zerolinecolor='#969696',
+                        zerolinewidth=4,
+                        linecolor='#636363',
+                        linewidth=4,
+
+                        showbackground=True,
+                        backgroundcolor='rgb(230, 230,230)'
+                        ),
+                        yaxis=dict(
+                        range=[-abs(yrange), abs(yrange)],
+                        title='X direction [mm]',
+                        titlefont=dict(
+                        family='Arial, sans-serif',
+                        size=18,
+                        color='black'
+                        ),
+                        gridcolor='#bdbdbd',
+                        gridwidth=2,
+                        zerolinecolor='#969696',
+                        zerolinewidth=4,
+                        linecolor='#636363',
+                        linewidth=4,
+
+                        showbackground=True,
+                                                                                                 
+                        backgroundcolor='rgb(230, 230, 230)'
+                        ),
+                        zaxis=dict(
+                        range=[-abs(zrange), abs(zrange)],
+                        title='Y direction [mm]',
+                        titlefont=dict(
+                        family='Arial, sans-serif',
+                        size=18,
+                        color='black'
+                        ),
+                        gridcolor='#bdbdbd',
+                        gridwidth=2,
+                        zerolinecolor='#969696',
+                        zerolinewidth=4,
+                        linecolor='#636363',
+                        linewidth=4,
+
+                        showbackground=True,
+                        backgroundcolor='rgb(230, 230,230)'
+                        ),
+                        aspectratio = dict(x=1, y=1, z=0.7),
+                        aspectmode = 'manual'
                         )
+                )
 
-                return {'data': [traceB], 'layout': go.Layout(hovermode = 'closest',
-                                        scene={'xaxis':{'title':''},'yaxis':{'title':'thetal'},'zaxis':{'title':'chi'}},
-                                        )
+
+
+
+
+
+
+
+                return {'data': [traceB,traceDst,tracetau,traceD0,tracenuB,traceK], 'layout':layout_event
 
                                 }
+
+
 
 
 
