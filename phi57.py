621b13d0c4d7826cfbf922ff46f1467c8706947f
wqke <47601667+wqke@users.noreply.github.com>
2019-03-14 15:14:02 +0100

    Update phi57.py

--- phi57.py
+++ phi57.py
@@ -61,10 +61,75 @@ chi = np.arctan2(si,co)
 costhetast=unitD0.dot(unitDst)
 costhetal=unitDst.dot(unittau)
 q2=(B-Dst).mag2
+
 q2=q2[~np.isnan(chi)]
+costhetal=costhetal[~np.isnan(chi)]
+costhetast=costhetast[~np.isnan(chi)]
 chi= chi[~np.isnan(chi)]
 
 
-q2_heights, q2_borders, _=plt.hist(q2,bins=np.linspace(min(q2),max(q2),11)) 
+q2_1=list(set(q2[costhetast>0.9]) & set(q2[costhetast<=1]))
+chi_1=list(set(chi[costhetast>0.9]) & set(chi[costhetast<=1]))
+
+chi_0=list(set(chi[costhetast<0.07171]) & set(chi[costhetast>-0.07171]))
+q2_0=list(set(q2[costhetast<0.07171]) & set(q2[costhetast>-0.07170]))
+
+chi_m1=list(set(chi[costhetast<-0.8852]) & set(chi[costhetast>=-1]))
+q2_m1=list(set(q2[costhetast<-0.8852]) & set(q2[costhetast>=-1]))
+
+q2_heights, q2_borders, _=plt.hist(q2,bins=4) 
 q2_centers = q2_borders[:-1] + np.diff(q2_borders) / 2
 plt.close()
+
+
+x0, y0 = chi_0,q2_0
+hist0, xedges0, yedges0 = np.histogram2d(x0, y0, bins=4)
+plt.close()
+
+x1, y1 = chi_1,q2_1
+hist1, xedges1, yedges1 = np.histogram2d(x1, y1, bins=4)
+plt.close()
+
+xm1, ym1 = chi_m1,q2_m1
+histm1, xedgesm1, yedgesm1 = np.histogram2d(xm1, ym1, bins=4)
+plt.close()
+
+x_centers1= xedges1[:-1] + np.diff(xedges1)/2
+y_centers1 = yedges1[:-1] + np.diff(yedges1)/2
+x_centers0 = xedges0[:-1] + np.diff(xedges0)/2
+y_centers0 = yedges0[:-1] + np.diff(yedges0)/2
+x_centersm1 = xedgesm1[:-1] + np.diff(xedgesm1)/2
+y_centersm1 = yedgesm1[:-1] + np.diff(yedgesm1)/2
+
+
+phi57=hist0*2-hist1-histm1
+
+"""""""
+
+q1=list(set(q2[chi>-0.1]) & set(q2[costhetast<0.1]) & set(q2[chi<0.1]) &set(q2[chi>-0.1]))
+q22=list(set(q2[costhetast>-0.1]) & set(q2[costhetast<0.1]) & set(q2[chi<min(chi)+0.188]) &set(q2[chi>=min(chi)]))
+q3=list(set(q2[costhetast>=-1]) & set(q2[costhetast<-0.9]) & set(q2[chi<0.15]) &set(q2[chi>-0.15]))
+q4=list(set(q2[costhetast>=-1]) & set(q2[costhetast<-0.85]) & set(q2[chi<min(chi)+0.1998]) &set(q2[chi>=min(chi)]))
+
+q5=list(set(q2[costhetast>0.85]) & set(q2[costhetast<=1]) & set(q2[chi<=max(chi)]) &set(q2[chi>max(chi)-0.187]))
+q6=list(set(q2[costhetast>-0.09]) & set(q2[costhetast<=0.09]) & set(q2[chi<=max(chi)]) &set(q2[chi>max(chi)-0.2085]))
+q7=list(set(q2[costhetast>0.8695]) & set(q2[costhetast<1]) & set(q2[chi<=max(chi)]) &set(q2[chi>max(chi)-0.2085]))
+q8=list(set(q2[costhetast>0.8697]) & set(q2[costhetast<1]) & set(q2[chi<0.07]) &set(q2[chi>-0.0964]))
+
+q9=list(set(q2[costhetast>-0.1]) & set(q2[costhetast<0.1]) & set(q2[chi<np.pi]) &set(q2[chi>np.pi-0.1926]))
+q10=list(set(q2[costhetast>-1]) & set(q2[costhetast<-0.9]) & set(q2[chi<0.1]) &set(q2[chi>-0.2]))
+q11=list(set(q2[costhetast>-0.1]) & set(q2[costhetast<0.1]) & set(q2[chi<0.1]) &set(q2[chi>-0.1]))
+q12=list(set(q2[costhetast>-1]) & set(q2[costhetast<-0.85]) & set(q2[chi<np.pi]) &set(q2[chi>np.pi-0.2059]))
+
+
+q13=list(set(q2[costhetast>-0.1]) & set(q2[costhetast<0.1]) & set(q2[chi<-np.pi+0.188]) &set(q2[chi>-np.pi]))
+q14=list(set(q2[costhetast>0.89]) & set(q2[costhetast<1]) & set(q2[chi<0.1]) &set(q2[chi>-0.0977]))
+q15=list(set(q2[costhetast>-0.1]) & set(q2[costhetast<0.1]) & set(q2[chi<0.1]) &set(q2[chi>-0.0999]))
+q16=list(set(q2[costhetast>0.83]) & set(q2[costhetast<1]) & set(q2[chi<0.1562-np.pi]) &set(q2[chi>-np.pi]))
+
+heights=plt.hist(q1,bins=4)[0]-plt.hist(q22,bins=4)[0]-plt.hist(q3,bins=4)[0]+plt.hist(q4,bins=4)[0]+plt.hist(q5,bins=4)[0]-plt.hist(q6,bins=4)[0]-plt.hist(q7,bins=4)[0]+plt.hist(q8,bins=4)[0]-plt.hist(q9,bins=4)[0]-plt.hist(q10,bins=4)[0]+plt.hist(q11,bins=4)[0]+plt.hist(q12,bins=4)[0]-plt.hist(q13,bins=4)[0]-plt.hist(q14,bins=4)[0]+plt.hist(q15,bins=4)[0]+plt.hist(q16,bins=4)[0] 
+
+
+
+
+
