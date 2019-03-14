35360306530daef05637aab449bc3999d8f235b0
wqke <47601667+wqke@users.noreply.github.com>
2019-03-14 16:12:25 +0100

    Update RLT.py

--- RLT.py
+++ RLT.py
@@ -62,7 +62,7 @@ costhetast=unitD0.dot(unitDst)
 costhetal=unitDst.dot(unittau)
 q2=(B-Dst).mag2
 
-q2_heights, q2_borders, _=plt.hist(q2,bins=np.linspace(min(q2),max(q2),5)) 
+q2_heights, q2_borders, _=plt.hist(q2,bins=np.linspace(min(q2),max(q2),11)) 
 q2_centers = q2_borders[:-1] + np.diff(q2_borders) / 2
 plt.close()
 
@@ -74,9 +74,9 @@ q2list=q2_centers
 RLTlist=[]
 RLTerr=[]
 q2err=[]
-for i in range(4):
+for i in range(10):
   set1=list(set(costhetast[q2>q2_borders[i]]) & set(costhetast[q2<q2_borders[i+1]]))
-  bin_heights, bin_borders, _=plt.hist(set1,bins=4,density=1/q2_heights[i])
+  bin_heights, bin_borders, _=plt.hist(set1,bins=10,density=1/q2_heights[i])
   bin_centers = bin_borders[:-1] + np.diff(bin_borders) / 2
   popt, pcov = curve_fit(fitRLT, bin_centers, bin_heights)
   a,c=(popt[0],popt[1])
@@ -85,7 +85,7 @@ for i in range(4):
   aerr,cerr=np.sqrt(np.diag(pcov))
   errz=rlt*np.sqrt( 2*aerr**2/(a+c)**2+aerr**2/(a)**2)
   RLTerr.append(errz)
-  q2err.append((max(q2)-min(q2))/8.)
+  q2err.append((max(q2)-min(q2))/20.)
   plt.close()
 
   
